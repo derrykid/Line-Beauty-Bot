1bbc11586b0601fbd677bc88fe8e38051d100df0
derrykid@github.com <tzuderry@gmail.com>
2022-03-31 18:14:06 +0800

    first commit

---
+++ src/main/java/com/example/LineTestBot/LineTestBotApplication.java
@@ -0,0 +1,29 @@
+package com.example.LineTestBot;
+
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@LineMessageHandler
+@Slf4j
+public class LineTestBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineTestBotApplication.class, args);
+	}
+
+	@EventMapping
+	public Message handleTextMessage(MessageEvent<TextMessageContent> event){
+		log.info("Event: " + event);
+
+		return new TextMessage(event.getMessage().getText());
+	}
+
+}
---
+++ src/test/java/com/example/LineTestBot/LineTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LineTestBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineTestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
