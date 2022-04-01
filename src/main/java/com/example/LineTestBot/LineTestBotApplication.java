b35247c6d028376c9e5d0267d72303d7c069ed34
derrykid@github.com <tzuderry@gmail.com>
2022-04-01 11:42:18 +0800

    line messaging client test

--- src/main/java/com/example/LineTestBot/LineTestBotApplication.java
+++ src/main/java/com/example/LineTestBot/LineTestBotApplication.java
@@ -1,32 +1,48 @@
 package com.example.LineTestBot;
 
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @LineMessageHandler
+@AllArgsConstructor
 @Slf4j
 public class LineTestBotApplication {
+
+	private String channel_token = "N6UpY0AcuaoeOd4g3YYL3DNqXF8tzIGcaXZ4oAWF8Wa+S4tIwhbufl15UCkS+am82kxgM8rBnRyXwgwYhIY1hmu+kh8NCckUZNRImthycZFA7dv5Oljwns8e117Bon2rOfM+uyfe84vSjk+Y7tkBigdB04t89/1O/w1cDnyilFU=";
+	private final LineMessagingClient lineMessagingClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LineTestBotApplication.class, args);
 	}
 
-	@EventMapping
-	public Message handleTextMessage(MessageEvent<TextMessageContent> event){
 
-		return new TextMessage("Hello");
+
+
+	@EventMapping
+	public void handleTextMessage(MessageEvent<TextMessageContent> event){
+		log(event);
+
+		lineMessagingClient.pushMessage(new PushMessage(event.getSource().getUserId(), new TextMessage("Hello, some update")));
+
 	}
 
-	@EventMapping
-	public void log(MessageEvent<TextMessageContent> event){
+	public static void log(MessageEvent<TextMessageContent> event){
 		log.info("Event: " + event);
 		System.out.println("Event Log: " + event);
 	}
