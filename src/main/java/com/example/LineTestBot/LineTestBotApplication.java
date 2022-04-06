3ef34fd1a63d0bf0deaf31909782fd1d50e08de7
derrykid@github.com <tzuderry@gmail.com>
2022-04-06 20:53:02 +0800

    push message

--- src/main/java/com/example/LineTestBot/LineTestBotApplication.java
+++ src/main/java/com/example/LineTestBot/LineTestBotApplication.java
@@ -7,6 +7,7 @@ import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.profile.UserProfileResponse;
 import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
@@ -23,59 +24,77 @@ import java.util.concurrent.ExecutionException;
 @Slf4j
 public class LineTestBotApplication {
 
+    static String derryID = "U3f3dc951e3cfb83333415a2df55f0fe1";
 
-	public static void main(String[] args) {
-		SpringApplication.run(LineTestBotApplication.class, args);
-	}
-
-
-	@EventMapping
-	public Message handleTextMessage(MessageEvent<TextMessageContent> event){
-		log(event);
-
-		if (event.getMessage().getText().equalsIgnoreCase("start")) {
-			TestOut.push(event);
-			return new TextMessage("Game start!");
-		}
-
-		return new TextMessage("Default");
-	}
-
-	public static void pushMessage(String userID) {
-		final LineMessagingClient client = LineMessagingClient
-				.builder("N6UpY0AcuaoeOd4g3YYL3DNqXF8tzIGcaXZ4oAWF8Wa+S4tIwhbufl15UCkS+am82kxgM8rBnRyXwgwYhIY1hmu+kh8NCckUZNRImthycZFA7dv5Oljwns8e117Bon2rOfM+uyfe84vSjk+Y7tkBigdB04t89/1O/w1cDnyilFU=")
-				.build();
-
-		final TextMessage textMessage = new TextMessage("Try out push functionality! I don't need bear");
-		final PushMessage pushMessage = new PushMessage( userID, textMessage);
-
-		final BotApiResponse botApiResponse;
-		try {
-			botApiResponse = client.pushMessage(pushMessage).get();
-		} catch (InterruptedException | ExecutionException e) {
-			e.printStackTrace();
-		}
-
-		final LineMessagingClient clientWithBear = LineMessagingClient
-				.builder("Bearer N6UpY0AcuaoeOd4g3YYL3DNqXF8tzIGcaXZ4oAWF8Wa+S4tIwhbufl15UCkS+am82kxgM8rBnRyXwgwYhIY1hmu+kh8NCckUZNRImthycZFA7dv5Oljwns8e117Bon2rOfM+uyfe84vSjk+Y7tkBigdB04t89/1O/w1cDnyilFU=")
-						.build();
-
-		final PushMessage push = new PushMessage(userID, new TextMessage("I need bearer QQ!"));
-		final BotApiResponse botApiResponse1;
-		try {
-			botApiResponse1 = client.pushMessage(push).get();
-		} catch (InterruptedException | ExecutionException e) {
-			e.printStackTrace();
-			return;
-		}
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LineTestBotApplication.class, args);
+    }
 
 
+    @EventMapping
+    public Message handleTextMessage(MessageEvent<TextMessageContent> event) {
+        log(event);
 
-	public static void log(MessageEvent<TextMessageContent> event){
-		log.info("Event: " + event);
-		System.out.println("Event Log: " + event);
-	}
+        if (event.getMessage().getText().equalsIgnoreCase("push")) {
+            pushMessage(event);
+            return null;
+        }
+
+        return new TextMessage("Default");
+    }
+
+    public static void pushMessage(MessageEvent<TextMessageContent> event) {
+        final LineMessagingClient client = LineMessagingClient
+                .builder("N6UpY0AcuaoeOd4g3YYL3DNqXF8tzIGcaXZ4oAWF8Wa+S4tIwhbufl15UCkS+am82kxgM8rBnRyXwgwYhIY1hmu+kh8NCckUZNRImthycZFA7dv5Oljwns8e117Bon2rOfM+uyfe84vSjk+Y7tkBigdB04t89/1O/w1cDnyilFU=")
+                .build();
+
+        String userName = getUserName(event);
+
+        final TextMessage textMessage = new TextMessage("This is your display name: " + userName);
+        final PushMessage pushMessage = new PushMessage(derryID, textMessage);
+
+        final BotApiResponse botApiResponse;
+        try {
+            botApiResponse = client.pushMessage(pushMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+//		final LineMessagingClient clientWithBear = LineMessagingClient
+//				.builder("Bearer N6UpY0AcuaoeOd4g3YYL3DNqXF8tzIGcaXZ4oAWF8Wa+S4tIwhbufl15UCkS+am82kxgM8rBnRyXwgwYhIY1hmu+kh8NCckUZNRImthycZFA7dv5Oljwns8e117Bon2rOfM+uyfe84vSjk+Y7tkBigdB04t89/1O/w1cDnyilFU=")
+//						.build();
+//
+//		final PushMessage push = new PushMessage(userID, new TextMessage("I need bearer QQ!"));
+//		final BotApiResponse botApiResponse1;
+//		try {
+//			botApiResponse1 = client.pushMessage(push).get();
+//		} catch (InterruptedException | ExecutionException e) {
+//			e.printStackTrace();
+//			return;
+//		}
+
+    }
+
+    private static String getUserName(MessageEvent<TextMessageContent> event) {
+        final LineMessagingClient client = LineMessagingClient
+                .builder("N6UpY0AcuaoeOd4g3YYL3DNqXF8tzIGcaXZ4oAWF8Wa+S4tIwhbufl15UCkS+am82kxgM8rBnRyXwgwYhIY1hmu+kh8NCckUZNRImthycZFA7dv5Oljwns8e117Bon2rOfM+uyfe84vSjk+Y7tkBigdB04t89/1O/w1cDnyilFU=")
+                .build();
+
+        final UserProfileResponse userProfileResponse;
+        try {
+            userProfileResponse = client.getProfile(event.getSource().getUserId()).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+        return "DisplayName method:" + userProfileResponse.getDisplayName() + "\n" ;
+    }
+
+
+    public static void log(MessageEvent<TextMessageContent> event) {
+        log.info("Event: " + event);
+        System.out.println("Event Log: " + event);
+    }
 
 }
--- src/main/java/com/example/LineTestBot/Message/TestOut.java
+++ src/main/java/com/example/LineTestBot/Message/TestOut.java
@@ -7,6 +7,5 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 public class TestOut {
     public static void push(MessageEvent<TextMessageContent> event) {
         String userID = event.getSource().getUserId();
-        LineTestBotApplication.pushMessage(userID);
     }
 }
