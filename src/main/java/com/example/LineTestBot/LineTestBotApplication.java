6a11c1e4e00ae44ffc899ecabb1da66e4188ff34
derrykid@github.com <tzuderry@gmail.com>
2022-04-08 16:27:52 +0800

    here

--- src/main/java/com/example/LineTestBot/LineTestBotApplication.java
+++ src/main/java/com/example/LineTestBot/LineTestBotApplication.java
@@ -31,6 +31,7 @@ public class LineTestBotApplication {
 
     static String derryID = "U3f3dc951e3cfb83333415a2df55f0fe1";
     static String anotherID = "Uc9e70cd5e5c151598d1c8fb58c7dbc3d";
+    static String test_token = "N6UpY0AcuaoeOd4g3YYL3DNqXF8tzIGcaXZ4oAWF8Wa+S4tIwhbufl15UCkS+am82kxgM8rBnRyXwgwYhIY1hmu+kh8NCckUZNRImthycZFA7dv5Oljwns8e117Bon2rOfM+uyfe84vSjk+Y7tkBigdB04t89/1O/w1cDnyilFU=";
 
     public static void main(String[] args) {
         SpringApplication.run(LineTestBotApplication.class, args);
@@ -75,7 +76,7 @@ public class LineTestBotApplication {
             connection.disconnect();
 
             // push stuff
-            String token = "4lHdgSOo/+RQsdLDmS3R99+HclBAXUAVFcCcgfF9FIrDzNiVWyOkfho59nsDahfnrfnkLPeVjDUkLB5Q9nj6A8WVgxMZ3DGRtsRO+hqZO6qoXzLcIKWBKvJhxkPc3Y1ok9etjDBGn7Hm1gmSEthktgdB04t89/1O/w1cDnyilFU=";
+            String token = test_token;
 
             LineMessagingClient client = LineMessagingClient.builder(token).build();
 
