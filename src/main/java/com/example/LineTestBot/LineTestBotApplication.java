348180e893b67cba6a498dc198796cae49912ad1
derrykid@github.com <tzuderry@gmail.com>
2022-04-08 15:53:41 +0800

    here

--- src/main/java/com/example/LineTestBot/LineTestBotApplication.java
+++ src/main/java/com/example/LineTestBot/LineTestBotApplication.java
@@ -41,35 +41,35 @@ public class LineTestBotApplication {
     public Message handleTextMessage(MessageEvent<TextMessageContent> event)  throws URISyntaxException, IOException, InterruptedException{
         log.info("event: " + event);
 
-        String targetURL =  "https://api.pokerapi.dev/v1/winner/texas_holdem?cc=AC,KD,QH,JS,7C&pc[]=10S,8C&pc[]=3S,2C";
-        try {
-            URL url = new URL( targetURL );
-            HttpURLConnection connection = (HttpURLConnection) url
-                    .openConnection();
-            /* 3. 設定請求引數（過期時間，輸入、輸出流、訪問方式），以流的形式進行連線 */
-            connection.setDoOutput(false);
-            connection.setDoInput(true);
-            connection.setRequestMethod("GET");
-            connection.setUseCaches(true);
-            connection.setInstanceFollowRedirects(true);
-            connection.connect();
-            int code = connection.getResponseCode();
-            String msg = "";
-            if (code == 200) { // 正常響應
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(connection.getInputStream()));
-                String line = null;
-
-                while ((line = reader.readLine()) != null) { // 迴圈從流中讀取
-                    msg += line + "\n";
-                }
-                reader.close(); // 關閉流
-            }
-            connection.disconnect();
-            return new TextMessage(msg);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        String targetURL =  "https://api.pokerapi.dev/v1/winner/texas_holdem?cc=AC,KD,QH,JS,7C&pc[]=10S,8C&pc[]=3S,2C";
+//        try {
+//            URL url = new URL( targetURL );
+//            HttpURLConnection connection = (HttpURLConnection) url
+//                    .openConnection();
+//            /* 3. 設定請求引數（過期時間，輸入、輸出流、訪問方式），以流的形式進行連線 */
+//            connection.setDoOutput(false);
+//            connection.setDoInput(true);
+//            connection.setRequestMethod("GET");
+//            connection.setUseCaches(true);
+//            connection.setInstanceFollowRedirects(true);
+//            connection.connect();
+//            int code = connection.getResponseCode();
+//            String msg = "";
+//            if (code == 200) { // 正常響應
+//                BufferedReader reader = new BufferedReader(
+//                        new InputStreamReader(connection.getInputStream()));
+//                String line = null;
+//
+//                while ((line = reader.readLine()) != null) { // 迴圈從流中讀取
+//                    msg += line + "\n";
+//                }
+//                reader.close(); // 關閉流
+//            }
+//            connection.disconnect();
+//            return new TextMessage(msg);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         return new TextMessage("This is hereee");
     }
 
