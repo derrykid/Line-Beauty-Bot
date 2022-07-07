346d0700742b7e8bce76eec2d3dac20428ceba87
derrykid@github.com <tzuderry@gmail.com>
2022-07-07 09:31:37 +0800

    remove pushMsg

--- src/main/java/com/example/LineTestBot/LineTestBotApplication.java
+++ src/main/java/com/example/LineTestBot/LineTestBotApplication.java
@@ -1,12 +1,9 @@
 package com.example.LineTestBot;
 
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import lombok.AllArgsConstructor;
@@ -14,17 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-
 @SpringBootApplication
 @LineMessageHandler
-@AllArgsConstructor
 @Slf4j
 public class LineTestBotApplication {
 
