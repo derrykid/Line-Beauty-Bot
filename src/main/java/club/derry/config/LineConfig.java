14bea68a214c3788b4dfb9fffdda042d20573e80
derrykid@github.com <tzuderry@gmail.com>
2022-07-08 15:44:22 +0800

    fix LineConfig bug

--- src/main/java/club/derry/config/LineConfig.java
+++ src/main/java/club/derry/config/LineConfig.java
@@ -11,12 +11,15 @@ public class LineConfig {
 
     private String channelToken;
     private String channelSecret;
+    private String handlerPath;
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     private LineConfig(@JsonProperty("channel-token") String token,
-                       @JsonProperty("channel-secret") String secret) {
+                       @JsonProperty("channel-secret") String secret,
+                        @JsonProperty("handler.path") String handlerPath) {
         this.channelToken = token;
         this.channelSecret = secret;
+        this.handlerPath = handlerPath;
     }
 
 }
